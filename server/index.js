d580b887caf2952d92298e8ba46ab40a6da70b22
nik frank <nikbfrank@gmail.com>
2019-08-01 07:02:04 -0700

    login and signup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
 const connectionString = 'postgres://yad3:guest@localhost:5432/yad3';
 
 const ORM = require('sequelize');
@@ -28,25 +31,65 @@ const Listing = connection.define('listing', {
   images: {
     type: ORM.ARRAY( ORM.TEXT ),
   },
+  author: {
+    type: ORM.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id',
+    },
+  },
+}, { freezeTableName: true });
+
+const User = connection.define('user', {
+  id: {
+    type: ORM.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  passwordHash: {
+    type: ORM.TEXT,
+    allowNull: false,
+  },
+  email: {
+    type: ORM.TEXT,
+    allowNull: false,
+    unique: true,
+  },
 }, { freezeTableName: true });
 
 app.use( express.json() );
+
+const auth = (req, res, next)=>{
+  const authHeader = req.get('Authorization') || '';
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, 'jwt secret code', (err, decoded)=>{
+    if( err ) return res.status(401).json({ message: 'auth failed' });
+    else {
+      req.session = decoded;
+      next();
+    }
+  });
+};
 
 connection.authenticate()
   .then(()=> console.log('success'))
   .catch(()=> console.log('failure'));
 
 app.get('/hydrate', (req, res)=> {
-  Listing.sync({ force: true })
-    .then(()=> res.json({ message: 'successfully created table' }))
-    .catch(err=> {
-      console.error(err);
-      res.status(500).json({ message: 'failed to create table' });
-    });
+  User.sync({ force: true })
+      .then(()=> Listing.sync({ force: true }))
+      .then(()=> res.json({ message: 'successfully created tables' }))
+      .catch(err=> {
+        console.error(err);
+        res.status(500).json({ message: 'failed to create table' });
+      });
 });
 
-app.post('/listing', (req, res)=> {
-  Listing.create(req.body)
+app.post('/listing', auth, (req, res)=> {
+  Listing.create({ ...req.body, id: req.session.id })
     .then((response)=>
       res.status(201).json({
         message: 'created',
@@ -65,6 +108,42 @@ app.get('/listing', (req, res)=> {
       console.error(err);
       res.status(500).json({ message: 'read listings failed' });
     });
+});
+
+const calculateHash = password => crypto.pbkdf2Sync(
+  password, 'secret code', 100, 64, 'sha512'
+).toString('hex');
+
+app.post('/user', (req, res)=> {
+  // sign up
+  const passwordHash = calculateHash( req.body.password );
+
+  User.create({
+    email: req.body.email,
+    passwordHash,
+  }).then(()=> res.status(201).json({ message: 'sign up successful' }))
+    .catch(()=> res.status(500).json({ message: 'sign up failed' }))
+});
+
+app.post('/login', (req, res)=> {
+  // login
+  const passwordHash = calculateHash( req.body.password );
+
+  User.findOne({ where: { email: req.body.email } })
+      .then(userResponse=> {
+        if( userResponse &&
+            (userResponse.dataValues.passwordHash === passwordHash) ){
+          // make jwt
+          jwt.sign(
+            { id: userResponse.dataValues.id },
+            'jwt secret code',
+            (err, token)=> res.json({ token })
+          );
+
+        } else {
+          res.status(401).json({ message: 'login failed' });
+        }
+      });
 });
 
 
